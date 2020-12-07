b283275c0e81335e84e72606cb8fd7d2b1898881
efeertugrul <ertugrulefecoskun@gmail.com>
2020-12-08 00:30:39 +0300

    changes

--- main.py
+++ main.py
@@ -2,15 +2,39 @@ import discord
 from discord.ext import commands
 import os
 import re
+import random
 
+def switch(value):
+    cases = {0:'Aleyküm selam ya müslüman!',
+            1:'ALLAHU EKBER!!!!',
+            2:'ilahi mi açtın mübarek?'}
+    return cases[value]
 
+on = 1
 client = commands.Bot(command_prefix=".")
 token = os.getenv("DISCORD_BOT_TOKEN")
+
+
+if token == None:
+    print("Za")
+    f = open("token","r")
+    token = f.readline()
+    print(token)
+else:
+    print(token)
+
 
 @client.event
 async def on_ready() :
     await client.change_presence(status = discord.Status.idle, activity = discord.Game("Listening to .help"))
     print("I am online")
+
+@client.command()
+async def seriat(ctx):
+    if on == 1:
+        on = 0
+    else:
+        on = 1
 
 @client.command()
 async def ping(ctx) :
@@ -24,15 +48,23 @@ async def whoami(ctx) :
 async def clear(ctx, amount=3) :
     await ctx.channel.purge(limit=amount)
 
-@client.event()
+@client.event
 async def on_message(message):
-    matched = re.match("sa|s.a.|selamın aleyküm|Selamın aleyküm", message)
-    matched2 = re.match("TEKBİR!", message)
-    if bool(matched):
-        await message.channel.send('Aleyküm selam ya müslüman!')
-    elif bool(matched2):
-        await message.channel.send('ALLAHU EKBER!!!!')
-    await bot.process_commands(message)
+    if on == 1:
+        matchlist = ["s.a|sa|s.a.|selamın aleyküm|Selamın aleyküm", "TEKBİR|tekbir", "-p"]
+
+        count = 0
+        for x in matchlist:
+            matched = re.match(x, message.content)
+            if(bool(matched)):
+                response = switch(count)
+                if count == 2:
+                    if random.randint(0, 100)%11!=0:
+                        break
+                await message.channel.send(response)
+                break
+            count += 1
+    await client.process_commands(message)
 
     
     
